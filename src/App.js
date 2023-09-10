d203fdfee1b494ea27a618d0bbf0109ea4837423
이은정 <leeeunjeong@ieunjeongs-MacBook-Pro.local>
2023-09-11 03:37:36 +0900

    pls...

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
    <>
-    <HashRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           {routes.map((route, index) => (
             <Route
@@ -22,7 +22,7 @@ function App() {
           ))}
            <Route path="*" element={<NotFound />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
       </>
   );
 }
